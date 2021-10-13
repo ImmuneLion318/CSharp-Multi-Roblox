30db1ffc04be3a47a7ee31e9d4d8f56ac03251a5
Immoon <54381449+ImmuneLion318@users.noreply.github.com>
2021-10-13 18:18:28 -0400

    Add files via upload

---
+++ Multi Roblox/Multi Roblox/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Multi_Roblox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Mutex(true, "ROBLOX_singletonMutex");
+
+            Console.Title = "Multi Roblox - ImmuneLion318#0001";
+            Console.SetWindowSize(80, 20);
+
+            string Watermark = @"
+  ___  ___      _ _   _  ______      _     _           
+  |  \/  |     | | | (_) | ___ \    | |   | |          
+  | .  . |_   _| | |_ _  | |_/ /___ | |__ | | _____  __
+  | |\/| | | | | | __| | |    // _ \| '_ \| |/ _ \ \/ /
+  | |  | | |_| | | |_| | | |\ \ (_) | |_) | | (_) >  < 
+  \_|  |_/\__,_|_|\__|_| \_| \_\___/|_.__/|_|\___/_/\_\
+                                                       
+                                                     ";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Watermark);
+            Console.ResetColor();
+            Console.WriteLine("  Keep This Console Open To Run Multiple Roblox Clients \n  If Closed Some Of The Clients Will Shutdown \n " +
+                " Each Client Must Have A Different Accounts Logged In");
+            Console.ReadLine();
+        }
+    }
+}
